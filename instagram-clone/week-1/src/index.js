2b1adffc638890b5d2a601d575f2ce54a5eb815c
Yuliana Hazda <yulianahazda@gmail.com>
2020-08-05 16:13:59 -0400

    Cleanup in useEffect hook

--- instagram-clone/week-1/src/index.js
+++ instagram-clone/week-1/src/index.js
@@ -4,70 +4,28 @@ import * as serviceWorker from './serviceWorker';
 
 
 function App() {
-  // const [language, setLanguage] = React.useState('python');
-  // const [yearsExperience, setYearsExperience] = React.useState(0);
-
-  // Concise way of useState
-  const [developer, setDeveloper] = React.useState({
-    name: "",
-    language: "PYTHON",
-    yearsExperience: 0,
-    isEmployed: false,
-  });
+  const [mousePosition, setMousePosition] = React.useState({ x: 0, y: 0 });
 
   React.useEffect(() => {
-    // Here we can access DOM API 
-    // Document.title has a dynamic value
-    document.title = developer.name;
-    console.log("runs");
-    // Note: naturally is going to run when the input is updated but it also going to run whenever any other piece of state is updated
-  }, [developer.name]);
+    document.addEventListener('mousemove', handleMouseMove);
 
-  // function handleInputChange(event) {
-  //   setInputValue(event.target.value);
-  // }
 
-  function handleChangeLanguage() {
-    setDeveloper({
-      language: "javascript",
-      yearsExperience: 2,
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    }
+  }, [])
+
+  function handleMouseMove(event) {
+    setMousePosition({
+      x: event.pageX,
+      y: event.pageY,
     })
   }
 
-  function handleChangeYearsExperience(event) {
-    setDeveloper({
-      ...developer,
-      yearsExperience: event.target.value,
-    })
-  }
-
-
-  function handleChangeTitle(e) {
-    setDeveloper({
-      ...developer,
-      name: e.target.value
-    })
-  }
-
-
-  function handleToggleEmployment() {
-    setDeveloper(prevState => ({
-      ...prevState,
-      isEmployed: !prevState.isEmployed,
-    }));
-  }
 
   return (
     <div>
-      <button onClick={handleToggleEmployment}>Toggle Employment Status</button>
-      <button onClick={handleChangeLanguage}>Change language</button>
-      <input
-        type="number"
-        onChange={handleChangeYearsExperience}
-      />
-      <input type="text" onChange={handleChangeTitle} placeholder="Change name" />
-      <p>I'm currently learning {developer.language} and I have {developer.yearsExperience} years of experience</p>
-      <p>Employment status: {developer.isEmployed ? "Employed" : "Unemployed"}</p>
+      <p>X: {mousePosition.x}, Y: {mousePosition.y}</p>
     </div>
   )
 }
@@ -86,6 +44,11 @@ ReactDOM.render(
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
+function Test() {
+  return (
+    <div>New page</div>
+  )
+}
 
 
-
+setTimeout(() => ReactDOM.render(<Test />, rootNode), 2000);
