ee3c8ecd070536a3c08956106a42d7d5b64bbd2b
Yuliana Hazda <yulianahazda@gmail.com>
2020-08-07 23:10:24 -0400

    Use React.createContexto pass props globally

--- instagram-clone/week-1/src/App.js
+++ instagram-clone/week-1/src/App.js
@@ -4,13 +4,14 @@ import Header from './components/Header';
 import CreatePost from './components/CreatePost';
 import PostList from './components/PostList';
 
-const functionsCount = new Set();
-
+// Helps to pass data down on
+// Every context is an object which consists of 2 properties: UserContext.provider &  UserContext.consumer
+// named export 
+export const UserContext = React.createContext();
 
 function App() {
     const [user, setUser] = useState('');
     const [posts, setPosts] = useState([]);
-    const [count, setCount] = useState(0);
 
 
     useEffect(() => {
@@ -21,30 +22,31 @@ function App() {
 
 
     // We might be updating the App state a lot, to fix that and for better performance we can use callback hook
-    // For 2 argument tell when to recreate this function, when out posts array changes
+    // For 2 argument tell when to recreate this function, when our posts array changes
     // React.useCallback memoized callback
+    // Usecallback remembers handleAddPost object and doesn't redeclare handleAddPost each re-render, only if dependencies have changed
     const handleAddPost = useCallback(
         newPost => {
-        setPosts([newPost, ...posts]);
-    }, [posts]);
+            setPosts([newPost, ...posts]);
+        }, [posts]);
 
-    functionsCount.add(handleAddPost);
-    console.log(functionsCount);
 
-    // if (!user) {
-    //     return <Login setUser={setUser} />;
-    // }
+
+    if (!user) {
+        return <Login setUser={setUser} />;
+    }
 
 
 
     return (
-        <>
+        <UserContext.Provider value={user}>
             <Header user={user} setUser={setUser} />
             <CreatePost user={user} handleAddPost={handleAddPost} posts={posts} />
             <PostList posts={posts} />
-            <button onClick={() => setCount(prev => prev + 1)}>{count} +</button>
-        </>
+        </UserContext.Provider>
     );
 }
 export default App;
+
+// Avoid pattern of passing props multiple levels called props drilling instead create context feature
 
--- instagram-clone/week-1/src/components/Post.js
+++ instagram-clone/week-1/src/components/Post.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import { UserContext } from '../App'
 
 function Post({ image, content, user }) {
 
+    // To consume a data we need to provide a functions as a child
+    // We return JSX which is wrapped in another fragment to make the code work
     return (
-        <>
-            <h1>Create a new Post</h1>
-            {image && <img
-                style={{ height: 100, width: 100, objectFit: 'cover' }}
-                src={URL.createObjectURL(image)}
-                alt="Post cover" />
-            }
-            <p>{content}</p>
-            <p>Posted by {user}</p>
-        </>
+        <UserContext.Consumer>
+            {(currentUser) => (
+                <>
+                    {image && <img
+                        style={{ height: 100, width: 100, objectFit: 'cover' }}
+                        src={URL.createObjectURL(image)}
+                        alt="Post cover" />
+                    }
+                    <p>{content}</p>
+                    <p style={{ color: currentUser === user && "green"}}>Posted by {user}</p>
+                </>
+            )}
+        </UserContext.Consumer>
     );
 }
 export default Post;
