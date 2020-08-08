629e8a07031844483936c0922a276f6ad251d710
Yuliana Hazda <yulianahazda@gmail.com>
2020-08-07 23:16:05 -0400

    Replace consumer with the useContext hook

--- instagram-clone/week-1/src/components/Post.js
+++ instagram-clone/week-1/src/components/Post.js
@@ -2,23 +2,20 @@ import React from 'react';
 import { UserContext } from '../App'
 
 function Post({ image, content, user }) {
+    const currentUser = React.useContext(UserContext);
 
     // To consume a data we need to provide a functions as a child
     // We return JSX which is wrapped in another fragment to make the code work
     return (
-        <UserContext.Consumer>
-            {(currentUser) => (
-                <>
-                    {image && <img
-                        style={{ height: 100, width: 100, objectFit: 'cover' }}
-                        src={URL.createObjectURL(image)}
-                        alt="Post cover" />
-                    }
-                    <p>{content}</p>
-                    <p style={{ color: currentUser === user && "green"}}>Posted by {user}</p>
-                </>
-            )}
-        </UserContext.Consumer>
+        <>
+            {image && <img
+                style={{ height: 100, width: 100, objectFit: 'cover' }}
+                src={URL.createObjectURL(image)}
+                alt="Post cover" />
+            }
+            <p>{content}</p>
+            <p style={{ color: currentUser === user && "green" }}>Posted by {user}</p>
+        </>
     );
 }
 export default Post;
