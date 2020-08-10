4745cfafab205d2ab4696bc2066d2f201caa01f2
Yuliana Hazda <yulianahazda@gmail.com>
2020-08-10 14:47:28 -0400

    Update Post component key prop

--- instagram-clone/week-1/src/components/PostList.js
+++ instagram-clone/week-1/src/components/PostList.js
@@ -4,7 +4,7 @@ import Post from './Post'
 function PostList({ posts }) {
     // Instead of passing the entire post object  post={post} we can simply spread in all properties of this object {...post}
     // We'll get directly from spread object image, content, user
-    return posts.map((post, i) => (<Post key={i} {...post} />));
+    return posts.map(post => (<Post key={post.id} {...post} />));
 }
 
 
