04fe6e70eac81bb7f98d506bb09db2dfb996ce44
Yuliana Hazda <yulianahazda@gmail.com>
2020-08-10 14:43:33 -0400

    Add logic for case delete_post

--- instagram-clone/week-1/src/reducer.js
+++ instagram-clone/week-1/src/reducer.js
@@ -2,12 +2,12 @@ function postReducer(state, action) {
     switch (action.type) {
         case "ADD_POST": {
             const newPost = action.payload.post;
-            return { posts: [newPost, ...state.posts] }
+            return { posts: [newPost, ...state.posts] };
         }
 
-        case "DELETE_POST": {
-
-        }
+        case "DELETE_POST":
+            const deletedPostId = action.payload.id;
+            return { posts: state.posts.filter(item => item.id !== deletedPostId) };
 
         default:
             return state;
